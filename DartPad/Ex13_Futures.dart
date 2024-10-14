ec0314b071e90b3f7f5ecdb773973e3196706288
Hugosago <Hugo.sago30@gmail.com>
2024-10-14 00:02:02 -0600

    Practica15
    Agregacion del ejercicio 13: Futures

---
+++ DartPad/Ex13_Futures.dart
@@ -0,0 +1,16 @@
+void main() {
+  print("Inicio del programa");
+  httpGet('https://hugo.com').then((value) {
+    print(value);
+  }).catchError((err) {
+    print("Error: $err");
+  });
+  print("Fin del programa");
+}
+
+Future<String> httpGet(String url) {
+  return Future.delayed(const Duration(seconds: 1), () {
+    throw 'Error en la petición https';
+    //return "Respuesta del servidor";
+  });
+}
